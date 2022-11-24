7466fcecb3fbf24bc94a758f8f74b708951d800a
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2022-11-24 12:04:33 +0100

    feat: add more optional arguments (#22)
    * feat: add more optional arguments
    
    * feat: fix indent

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -31,19 +31,18 @@ module "acr" {
 
   registry = {
     demo = {
-      location          = module.global.groups.acr.location
-      resourcegroup     = module.global.groups.acr.name
-      sku               = "Premium"
-      retention_in_days = 90
-
-      identity = {
-        type = "UserAssigned"
-      }
+      location            = module.global.groups.acr.location
+      resourcegroup       = module.global.groups.acr.name
+      sku                 = "Premium"
+      retention_in_days   = 90
+      network_rule_bypass = "None"
 
       enable = {
-        trust_policy     = true
-        retention_policy = true
-        admin            = true
+        trust_policy          = true
+        retention_policy      = true
+        admin                 = true
+        export_policy         = true
+        public_network_access = true
       }
 
       replications = {
